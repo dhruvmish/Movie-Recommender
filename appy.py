ef5566da246c6c2991fe8da8468d362384c5a3f1
dhruvmish <152987001+dhruvmish@users.noreply.github.com>
2025-06-28 21:50:14 +0530

    Update appy.py

--- appy.py
+++ appy.py
@@ -2,6 +2,9 @@ import pickle
 import streamlit as st
 import requests
 import pandas as pd
+import os
+from huggingface_hub import hf_hub_download
+
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
@@ -12,14 +15,27 @@ def recommend(movie):
         recommended_movies.append(movies.iloc[i[0]].title)
     return recommended_movies
 
+# Load movies_dict as before
 movies_dict = pickle.load(open('movies_dict.pkl','rb'))
 movies = pd.DataFrame(movies_dict)
 
-similarity = pickle.load(open('similarity.pkl', 'rb'))
+# Download similarity.pkl from Hugging Face if not present locally
+@st.cache_resource(show_spinner="Downloading similarity matrix from Hugging Face...")
+def load_similarity():
+    # This will download and cache the file in ~/.cache/huggingface/hub/
+    file_path = hf_hub_download(
+        repo_id="dhr-uuu34/movie-recommender",
+        filename="similarity.pkl"
+    )
+    with open(file_path, 'rb') as f:
+        similarity = pickle.load(f)
+    return similarity
+
+similarity = load_similarity()
 
 st.title("MOVIE RECOMMENDATIONS")
 
-selected_movie_name = st.selectbox('HOW WOULD YOU LIKE TO BE CONTACTED ?',movies['title'].values)
+selected_movie_name = st.selectbox('Select a movie:', movies['title'].values)
 
 if st.button('Recommend'):
     recommendations = recommend(selected_movie_name)
